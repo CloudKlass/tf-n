63251d853302fa582b13d4d6c4e7452138cad4a0
CloudKlass <104594551+CloudKlass@users.noreply.github.com>
2022-08-10 14:30:34 +0100

    L7update

--- solutions/lab7_build_n-tier_vpc/network/variables.tf
+++ solutions/lab7_build_n-tier_vpc/network/variables.tf
@@ -1,5 +1,5 @@
 variable "tag_name" {
-   default = "lab4-vpc"
+   default = "lab7-vpc"
 }
 
 variable "vpc-cidr" {
@@ -8,7 +8,7 @@ variable "vpc-cidr" {
 
 variable "labname" {
    description = "prefix for lab resources"
-   default = "lab4"
+   default = "lab7"
 }
 
 #map to create subnets : usw2 = US-WEST-2 (oregon) and this is followed by the desired AZ ie az1
